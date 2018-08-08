b8bc222eb9a353bbc8843ca1a34d3272907133c7
xiaopeng.liu <xiaopeng.liu@ptmind.com>
2018-08-09 02:05:03 +0800

    config server

---
+++ kncoast-web/kncoast-web-config-server/src/main/java/com/kncoast/ConfigServerApplication.java
@@ -0,0 +1,9 @@
+package com.kncoast;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+}

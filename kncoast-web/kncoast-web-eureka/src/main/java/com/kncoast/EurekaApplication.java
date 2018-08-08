f73daf2b7a2a0087ec52cdc777be946cc96281ec
xiaopeng.liu <xiaopeng.liu@ptmind.com>
2018-08-09 01:22:51 +0800

    init

--- kncoast-web/kncoast-web-eureka/src/main/java/com/kncoast/EurekaApplication.java
+++ kncoast-web/kncoast-web-eureka/src/main/java/com/kncoast/EurekaApplication.java
@@ -7,11 +7,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.ApplicationContext;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
-	
+
 	private static Logger logger = LoggerFactory.getLogger(EurekaApplication.class);
 
 	public static void main(String[] args) {
@@ -22,5 +25,16 @@ public class EurekaApplication {
 			env = activeProfiles[0];
 		}
 		logger.info("Current startup environment：" + env);
+	}
+}
+
+@EnableWebSecurity
+class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// 关闭csrf
+		http.csrf().disable();
+		super.configure(http);
 	}
 }
---
+++ kncoast-web/kncoast-web-zuul/src/main/java/com/kncoast/ZuulApplication.java
@@ -0,0 +1,25 @@
+package com.kncoast;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.context.ApplicationContext;
+
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulApplication {
+
+	private static Logger logger = LoggerFactory.getLogger(ZuulApplication.class);
+	
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(ZuulApplication.class, args);
+		String[] activeProfiles = ctx.getEnvironment().getActiveProfiles();
+		String env = "default";
+		if (!ArrayUtils.isEmpty(activeProfiles)) {
+			env = activeProfiles[0];
+		}
+		logger.info("Current startup environment：" + env);
+	}
+}
